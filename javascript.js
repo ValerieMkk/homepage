944fec0575993de8e81d638e75403153b03183e4
ValerieMkk <valeriiamyk@gmail.com>
2022-06-04 20:41:01 +0300

    Add javascript

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function funOnLoad() {
+	document.body.classList.add('loaded_hiding');
+
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 3600);
+}
